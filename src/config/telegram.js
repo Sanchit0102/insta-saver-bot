73bc1004b63b72da9577a51ba5179de7a964419b
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-18 21:56:54 +0530

    Update telegram.js

--- src/config/telegram.js
+++ src/config/telegram.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "7491863909:AAG6k3Jgjn4EnvjRdVGe2rcHM9Jw_i686HE";
+const token = process.env.TELEGRAM_TOKEN;;
 
 const Bot = new TelegramBot(token, { polling: true });
 
